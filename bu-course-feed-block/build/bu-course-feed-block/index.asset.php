22f4095394c87886c9928a45017f259765219ec9
hitesh <hht179@gmail.com>
2025-01-26 11:24:16 -0500

    workin1

--- bu-course-feed-block/build/bu-course-feed-block/index.asset.php
+++ bu-course-feed-block/build/bu-course-feed-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '82830b461af32f460d01');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '6438f79a6f2ac99ecda2');
