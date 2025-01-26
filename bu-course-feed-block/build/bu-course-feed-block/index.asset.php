fc364560dd51647cc8b666209bbcb285e0951df3
hitesh <hht179@gmail.com>
2025-01-26 11:07:59 -0500

    first commit

---
+++ bu-course-feed-block/build/bu-course-feed-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'df65c59facbc8a6b8277');
---
+++ bu-course-feed-block/build/bu-course-feed-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '0f229e799a257ec995b0');
